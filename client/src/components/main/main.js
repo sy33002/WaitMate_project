27da0bc19033e653a46deb3cb2fb76b5cdc710a6
정채림 <119480528+ellin45@users.noreply.github.com>
2023-10-26 22:23:29 +0900

    "feat: 메인 수정중"

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -8,35 +8,60 @@ export default function Main() {
   });
 
   return (
-    <div className="flex flex-col space-y-4">
-      <img
-        src="/images/logo.png"
-        alt="로고"
-        className="items-center w-12 md:w-24 sm:w-20"
-      />
-      <div className="flex relative" data-aos="fade-up-right">
-        <img src="/images/Main_Union.png" alt="도형" className="" />
-        <div className="absolute inset-0 left-32 flex justify-start items-center text-primary text-7xl">
-          More Service Less Time
+    <>
+      <div className="flex justify-between items-start h-full">
+        <div className="flex flex-col space-y-4 w-4/5">
+          <div
+            className="flex justify-center"
+            data-aos="fade-down"
+            data-aos-delay="1200"
+          >
+            <img
+              src="/images/logo.png"
+              alt="로고"
+              className="w-60 h-60 sm:w-30 sm:h-30 md:w-48 md:h-48 lg:w-60 lg:h-60"
+            />
+          </div>
+          <div
+            className="flex relative top-12"
+            data-aos="fade-right"
+            data-aos-delay="1000"
+          >
+            <img src="/images/Main_Union.png" alt="도형" className='w-[260px] h-[100px]'/>
+            <div className="absolute inset-0 left-20 flex justify-start items-center text-primary text-shadow-lg font-bold text-5xl sm:text-3xl md:text-5xl lg:text-7xl">
+              More Service Less Time
+            </div>
+          </div>
+        </div>
+
+        {/* 보라색 div */}
+        <div
+          style={{ width: '60vw', height: '100vh', backgroundColor: '#4B4097' }}
+          className="relative"
+        ></div>
+        <div className="flex">
+          {/* 로그인 버튼 */}
+          <div className="absolute bottom-72 right-32 ">
+            <button
+              className="bg-primary rounded-sm px-6 py-3 text-white"
+              data-aos="fade-left"
+              data-aos-delay="900"
+            >
+              로그인
+            </button>
+          </div>
+          {/* 카카오 로그인 버튼 */}
+          <div className="absolute bottom-52 right-32 ">
+            <button
+              className="bg-yellow rounded-sm px-6 py-3"
+              data-aos="fade-left"
+              data-aos-delay="1100"
+            >
+              카카오 로그인
+            </button>
+          </div>
         </div>
       </div>
-      <div className="relative">
-        <img
-          src="/images/Main_Polygon.png"
-          alt="도형"
-          className="absolute right-0 top-0"
-        />
-        <div className="absolute top-56 right-8 ">
-          <button className="bg-primary rounded-sm px-6 py-3 text-white">
-            로그인
-          </button>
-        </div>
-        <div className="absolute top-72 right-8 ">
-          <button className="bg-yellow rounded-sm px-6 py-3">
-            카카오 로그인
-          </button>
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -8,13 +8,18 @@ module.exports = {
       colors: {
         primary: '#2F2772',
         background: '#FFFFF8',
-        yellow : '#FEE500',
+        yellow: '#FEE500',
       },
       spacing: {
-        '48': '12rem',
-        '52': '13rem',
-        '56': '14rem',
-      }
+        48: '12rem',
+        52: '13rem',
+        56: '14rem',
+      },
+      textShadow: {
+        default: '2px 2px 4px rgba(0, 0, 0, 0.1)',
+        lg: '2px 2px 5px rgba(0, 0, 0, 0.2)',
+        // 추가 원하는 그림자 스타일을 여기에 정의할 수 있습니다.
+      },
     },
   },
   plugins: [],
