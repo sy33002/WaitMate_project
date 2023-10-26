9e8ec91decbe85375b4b0f3d99aba97b52d7c76c
정채림 <119480528+ellin45@users.noreply.github.com>
2023-10-26 14:02:38 +0900

    "feat: prettier singleQuote 적용"

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -1,16 +1,13 @@
-import React from "react";
+import React from 'react';
 
 export default function main() {
   return (
-
-      <div className="flex flex-col items-center space-y-4">
+    <div className="flex flex-col items-center space-y-4">
       <img src="/images/logo.png" alt="로고" className="w-12 md:w-24 sm:w-20" />
       <div>WAIT MATE</div>
       <div className="text-center">More Service Less Time</div>
       <button className="bg-primary rounded-sm px-4 py-2">로그인</button>
       <button className="bg-yellow rounded-sm px-4 py-2">카카오 로그인</button>
-    
-
     </div>
   );
 }
