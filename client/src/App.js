d917f7f2874edd3a74234be8c837f17ced7f3418
정채림 <119480528+ellin45@users.noreply.github.com>
2023-10-27 18:07:53 +0900

    "feat: env 추가 및 메인 페이지 수정"

--- client/src/App.js
+++ client/src/App.js
@@ -5,18 +5,18 @@ import Main from './components/main/Main';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'; // Route를 추가로 임포트
 
 function App() {
-  return <div className='bg-background w-full h-screen'>
+  return (
+    <div className="bg-background w-full h-screen">
       <BrowserRouter>
-
-      <Main/>
-      <Routes>
-        <Route Header />
-        <Route Sidebar />
-        <Route Footer />
+        <Main />
+        <Routes>
+          <Route Header />
+          <Route Sidebar />
+          <Route Footer />
         </Routes>
       </BrowserRouter>
     </div>
-  
+  );
 }
 
 export default App;
--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import '/Users/user/code/2nd_project/front-wait/client/src/components/main/main.css';
 
 export default function Main() {
   useEffect(() => {
@@ -11,35 +12,51 @@ export default function Main() {
     <>
       <div className="flex justify-between items-start h-full">
         <div className="flex flex-col space-y-4 w-4/5">
+          {/* 로고 */}
           <div
-            className="flex justify-center"
+            className="absolute left-1/2 z-10"
             data-aos="fade-down"
             data-aos-delay="1200"
           >
             <img
-              src="/images/logo.png"
+              src="/images/logo_clock.png"
               alt="로고"
+              className="w-60 h-60 sm:w-30 sm:h-30 md:w-48 md:h-48 lg:w-60 lg:h-60 rotate-animation"
+            
+            />
+            <img
+              src="/images/logo_letter.png"
+              alt="로고_글씨"
               className="w-60 h-60 sm:w-30 sm:h-30 md:w-48 md:h-48 lg:w-60 lg:h-60"
             />
           </div>
+          {/* 도형 + 메세지 */}
           <div
             className="flex relative top-12"
             data-aos="fade-right"
             data-aos-delay="1000"
           >
-            <img src="/images/Main_Union.png" alt="도형" className='w-[260px] h-[100px]'/>
+            <img
+              src="/images/Main_Union.png"
+              alt="도형"
+              className="w-[1650px] h-[406px]"
+            />
             <div className="absolute inset-0 left-20 flex justify-start items-center text-primary text-shadow-lg font-bold text-5xl sm:text-3xl md:text-5xl lg:text-7xl">
               More Service Less Time
             </div>
           </div>
         </div>
 
-        {/* 보라색 div */}
-        <div
-          style={{ width: '60vw', height: '100vh', backgroundColor: '#4B4097' }}
-          className="relative"
-        ></div>
         <div className="flex">
+          {/* 보라색 div */}
+          <div
+            style={{
+              width: '60vw',
+              height: '100vh',
+              backgroundColor: '#4B4097',
+            }}
+            className="relative"
+          ></div>
           {/* 로그인 버튼 */}
           <div className="absolute bottom-72 right-32 ">
             <button
@@ -56,6 +73,9 @@ export default function Main() {
               className="bg-yellow rounded-sm px-6 py-3"
               data-aos="fade-left"
               data-aos-delay="1100"
+              onClick={() => {
+                window.location.href = `https://kauth.kakao.com/oauth/authorize?redirect_uri=http://localhost:8080/user/kakao&client_id=${process.env.REACT_APP_KAKAO_REST_API_KEY}&response_type=code`;
+              }}
             >
               카카오 로그인
             </button>
