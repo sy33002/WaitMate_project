3001070dd6568904940988470ba7221872e73ea3
정채림 <chaelimj45@gmail.com>
2023-10-28 02:40:06 +0900

    "feat: 반응형 수정중"

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import Template from './static/Template';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import ProxyRegister from './components/proxy/ProxyRegister';
 import ProxyList from './components/proxy/ProxyList';
-
+import Main from './components/main/Main';
 function App() {
   return (
     <div className="bg-background">
     <BrowserRouter>
       <Routes>
-        <Route path='/' element='Main' />
+        <Route path='/' element={<Main></Main>} />
         <Route path='/proxyRegister' element={<Template><ProxyRegister /></Template>}></Route>
         <Route path='/proxyList' element={<Template><ProxyList /></Template>}></Route>
       </Routes>
--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -6,29 +6,23 @@ export default function Main() {
   useEffect(() => {
     AOS.init();
   });
-
-export default function Main() {
   return (
     <>
       <div className="flex justify-between items-start h-full">
-        <div className="flex flex-col space-y-4 w-4/5">
+        <div className="flex flex-col space-y-4 w-3/5">
           {/* 로고 */}
           <div
-            className="absolute left-1/2 z-10"
+            className="absolute left-1/3 z-10"
             data-aos="fade-down"
             data-aos-delay="1200"
           >
             <img
-              src="/images/logo_clock.png"
+              src="/images/logo.png"
               alt="로고"
-              className="w-80 h-80 sm:w-30 sm:h-30 md:w-48 md:h-48 lg:w-80 lg:h-80 "
+              className="w-70 h-70 sm:w-40 sm:h-40 md:w-48 md:h-48 lg:w-70 lg:h-70 "
             
             />
-            {/* <img
-              src="/images/logo_letter.png"
-              alt="로고_글씨"
-              className="w-30 h-30 sm:w-10 sm:h-10 md:w-20 md:h-20 lg:w-30 lg:h-30"
-            /> */}
+           
           </div>
           {/* 도형 + 메세지 */}
           <div
@@ -41,7 +35,7 @@ export default function Main() {
               alt="도형"
               className="w-[1450px] h-[406px]"
             /> */}
-            <div className="absolute inset-0 left-20 flex justify-start items-center text-primary text-shadow-lg font-bold text-5xl sm:text-3xl md:text-5xl lg:text-7xl">
+            <div className="absolute inset-0 left-20 flex justify-start items-center text-primary text-shadow-lg font-bold text-5xl sm:text-4xl md:text-5xl lg:text-7xl">
               More Service Less Time
             </div>
           </div>
@@ -51,14 +45,14 @@ export default function Main() {
           {/* 보라색 div */}
           <div
             style={{
-              width: '20vw',
+              width: '30vw',
               height: '100vh',
               backgroundColor: '#4B4097',
             }}
             className="relative"
           ></div>
           {/* 로그인 버튼 */}
-          <div className="absolute bottom-52 right-32 md:bottom-32 md:right-32 sm:bottom-72 sm:right-32">
+          <div className="absolute bottom-52 right-32 md:bottom-52 md:right-32 sm:bottom-52 sm:right-32">
             <button
               className="bg-primary rounded-md px-8 py-4 text-white"
               data-aos="fade-left"
@@ -68,7 +62,7 @@ export default function Main() {
             </button>
           </div>
           {/* 카카오 로그인 버튼 */}
-          <div className="absolute bottom-52 right-32 ">
+          <div className="absolute bottom-32 right-32 md:bottom-32 md:right-32 sm:bottom-32 sm:right-32">
             <button
               className="bg-yellow rounded-md px-8 py-4"
               data-aos="fade-left"
