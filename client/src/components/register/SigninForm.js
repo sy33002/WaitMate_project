a320c981e784f5bf8d9bd9159d012fe790879b78
Vamos-Hyuk <kyle910912@gmail.com>
2023-10-30 21:34:33 +0900

    feat: 로그인 기능 구현

--- client/src/components/register/SigninForm.js
+++ client/src/components/register/SigninForm.js
@@ -1,12 +1,35 @@
 import React, { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 function SigninForm() {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
-  const handleSubmit = (e) => {
+  const navigate = useNavigate(); // useNavigate를 추가해줍니다.
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(username, password);
+
+    try {
+      const response = await axios.post('/api/users/login', {
+        // 백엔드 API 경로를 수정해야 합니다.
+        username,
+        password,
+      });
+
+      if (response.status === 200) {
+        // 로그인 성공 시 필요한 로직 (예: 페이지 리다이렉트)
+      }
+    } catch (error) {
+      if (error.response && error.response.data) {
+        // 백엔드에서 반환하는 오류 메시지를 설정합니다.
+        setErrorMessage(error.response.data.message);
+      } else {
+        setErrorMessage('로그인 중 오류가 발생했습니다.');
+      }
+    }
   };
 
   return (
@@ -14,6 +37,7 @@ function SigninForm() {
       <div className="p-4 w-1/4 shadow-lg background rounded-md">
         <h1 className="text-2xl text-primary mb-4">Sign In</h1>
         <p className="mb-4 text-gray-400">Wellcome!</p>
+        {errorMessage && <p className="text-red-500">{errorMessage}</p>}
         <form onSubmit={handleSubmit} className="flex flex-col items-center">
           <input
             type="text"
@@ -36,11 +60,18 @@ function SigninForm() {
             >
               Log In
             </button>
-            <button className="p-2 w-60 bg-background text-primary rounded-md border-2 border-primary shadow-lg">
+            <button
+              className="p-2 w-60 bg-background text-primary rounded-md border-2 border-primary shadow-lg"
+              onClick={() => navigate('/register/SignupForm')} // 'Sign Up' 버튼 클릭 시 회원가입 페이지로 이동합니다.
+            >
               Sign Up
             </button>
           </div>
-          <p className="text-center mt-2">아이디/비밀번호 찾기</p>
+          <p className="text-center mt-2">
+            <a href="/password-reset" className="text-primary hover:underline">
+              아이디/비밀번호 찾기
+            </a>
+          </p>
         </form>
       </div>
     </div>
