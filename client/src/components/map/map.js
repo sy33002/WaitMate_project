0186de38e3bcbcd8c51e43854db3940163fcf266
정채림 <119480528+ellin45@users.noreply.github.com>
2023-11-06 16:02:41 +0900

    "feat: map 표시 완료"

--- client/src/components/map/map.js
+++ client/src/components/map/map.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
-export default function MapComponent({ locationInfo }) {
+export default function MapComponent() {
   const [userLocation, setUserLocation] = useState(null);
+  const [userAddress, setUserAddress] = useState([]);
+  const { wmId } = useParams();
 
   // 사용자의 현재 위치를 가져오는 함수
   function getCurrentLocation(callback) {
@@ -26,11 +30,22 @@ export default function MapComponent({ locationInfo }) {
     });
   }, []);
 
-  const hardcodedLocationInfo = {
-    marker1: { lat: 37.123, lng: 127.456 },
-    marker2: { lat: 38.789, lng: 128.012 },
-  };
-  
+  useEffect(() => {
+    axios.get(`http://localhost:8080/waitMate/mapList`)
+      .then((res) => {
+        const data = res.data;
+        setUserAddress(data);
+        console.log('waitMate 데이터:', data);
+        data.forEach((data)=>{
+          console.log("data.lat",data.lat);
+        })
+      })
+      .catch((error) => {
+        // 요청이 실패했을 때 에러 처리
+        console.error('데이터 가져오기 실패:', error);
+      });
+  }, [wmId]); // wmId가 변경될 때마다 다시 요청
+
   return (
     <div>
       <Map
@@ -51,16 +66,15 @@ export default function MapComponent({ locationInfo }) {
           />
         )}
 
-        {/* locationInfo의 데이터를 마커로 표시 */}
-        {Object.keys(locationInfo).map((key, index) => {
-          const data = locationInfo[key];
-          console.log("data", data)
-          if (data && data.lat && data.lng) {
+        {/* userAddress의 데이터를 마커로 표시 */}
+        {userAddress.map((data, index) => {
+          console.log('data', data);
+          if (data.lat && data.lng) {
             return (
               <MapMarker
                 key={index}
                 position={{ lat: data.lat, lng: data.lng }}
-                text={key} // 각 데이터의 텍스트 표시 설정 (원하는 대로 수정 가능)
+                text={data.key} // 각 데이터의 텍스트 표시 설정 (원하는 대로 수정 가능)
                 image={{
                   src: './images/waitMate.png',
                   size: { width: 64, height: 64 },
