db77fad3ed829b5758d22d0a5311ce5449494301
Vamos-Hyuk <kyle910912@gmail.com>
2023-11-06 21:22:56 +0900

    useUserStore연결

--- client/src/store/useUserStore.js
+++ client/src/store/useUserStore.js
@@ -1,37 +1,37 @@
-import create from 'zustand'
-import { axiosInstance } from '../components/common/axiosInstance'
+import create from 'zustand';
+import { axiosInstance } from '../components/common/axiosInstance';
 
 const useUserStore = create((set) => ({
-  id : '',
-  userId : '',
-  nickname : '',
-  profileImg : '',
-  setUserInfo : async () => {
+  id: '',
+  userId: '',
+  nickname: '',
+  profileImg: '',
+  setProfileImage: (newImage) => set(() => ({ profileImg: newImage })),
+  setUserInfo: async () => {
     if (useUserStore.getState().id === '') {
       const response = await axiosInstance.get('/user/myInfo');
-      const {id, userId, nickname, photo} = response.data;
-      set(({
-        id : id,
-        userId : userId,
-        nickname : nickname,
-        profileImg : photo
-      }));
+      const { id, userId, nickname, photo } = response.data;
+      set({
+        id: id,
+        userId: userId,
+        nickname: nickname,
+        profileImg: photo,
+      });
     }
   },
-  logout : async () => {
+  logout: async () => {
     const response = await axiosInstance.get('/user/logOut');
     if (response.status === 200) {
-      set(({
-        id : '',
-        userId : '',
-        nickname : '',
-        profileImg : ''
-      })); 
+      set({
+        id: '',
+        userId: '',
+        nickname: '',
+        profileImg: '',
+      });
     } else {
-      console.log('알 수 없는 서버 에러')
+      console.log('알 수 없는 서버 에러');
     }
+  },
+}));
 
-  }
-}))
-
-export default useUserStore
+export default useUserStore;
