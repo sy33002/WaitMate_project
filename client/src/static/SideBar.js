edeec1a7dc07fd54482d460a930fc182ced6de9d
kim_jihyung <sy33002@naver.com>
2023-11-07 17:48:29 +0900

    feat: sideBar 기능 추가

--- client/src/static/SideBar.js
+++ client/src/static/SideBar.js
@@ -4,20 +4,24 @@ import {proxySubmenu} from './SideBarSub';
 import { Link } from 'react-router-dom';
 import './sideBar.css';
 
-function SidebarItem({ text, subMenu }) {
-  const [isSubMenuOpen, setSubMenuOpen] = useState(false);
+function SidebarItem({ text, subMenu, activeSubMenu, setActiveSubMenu }) {
+  const isSubMenuOpen = activeSubMenu === text;
   const [isHovered, setHovered] = useState(false);
 
   const toggleSubMenu = () => {
-    setSubMenuOpen(!isSubMenuOpen);
+    if (isSubMenuOpen) {
+      setActiveSubMenu(null);
+    } else {
+      setActiveSubMenu(text);
+    }
   };
 
   return (
     <div>
-      <li className={`relative m-2 rounded-sm p-2 cursor-pointer font-gmarket transition-all duration-300 ${
-      isHovered ? 'text-background bg-primary_dark transition-transform transform translate-x-0 hover:translate-x-4' : 'text-primary bg-background'
+      <li className={`relative m-2 rounded-sm p-2 cursor-pointer font-gmarket font-bold transition-all duration-300 ${
+      isHovered ? 'text-green bg-primary_dark transition-transform transform translate-x-0 hover:translate-x-4' : 'text-primary_dark bg-background'
     } ${
-      isSubMenuOpen ? 'bg-primary shadow-lg text-green' : ''
+      isSubMenuOpen ? 'bg-red-200 shadow-lg ' : ''
     }`}
         onClick={toggleSubMenu}
         onMouseEnter={() => setHovered(true)}
@@ -32,6 +36,8 @@ function SidebarItem({ text, subMenu }) {
 }
 
 function Sidebar() {
+  const [activeSubMenu, setActiveSubMenu] = useState(null);
+
   return (
     <div className="p-4 bg-background w-58 h-screen mt-10">
       <ul className="text-lg xl:text-xl h-full flex flex-col justify-start item-center text-center">
@@ -39,26 +45,34 @@ function Sidebar() {
         <Link to='/waitMate/register'>
         <SidebarItem 
           text="Wait Mate" 
-          subMenu={wmSubmenu} />
+          subMenu={wmSubmenu} 
+          activeSubMenu={activeSubMenu}
+          setActiveSubMenu={setActiveSubMenu} />
         </Link>
         </div>
         <div className='h-1/5'>
         <Link to='/proxy/register'>
         <SidebarItem 
           text="Proxy"
-         subMenu={proxySubmenu} />
+          subMenu={proxySubmenu}
+          activeSubMenu={activeSubMenu}
+          setActiveSubMenu={setActiveSubMenu} />
         </Link>
         </div>
         <div className='h-1/5'>
         <Link to='/map'>
         <SidebarItem 
-          text="Map" />
+          text="Map" 
+          activeSubMenu={activeSubMenu}
+          setActiveSubMenu={setActiveSubMenu} />
         </Link>
         </div>
         <div className='h-1/5'>
         <Link to='/myPage/Chatlist'>
         <SidebarItem 
-          text="My Chat"/>
+          text="My Chat"
+          activeSubMenu={activeSubMenu}
+          setActiveSubMenu={setActiveSubMenu} />
         </Link>
         </div>
       </ul>
