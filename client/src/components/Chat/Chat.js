6033adf3edf538dc793db0868faaf2c111401ba0
LOBSTER10000 <ghh112034@gmail.com>
2023-11-10 18:16:02 +0900

    fix : 채팅방 && 테스트 아이디 변경

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -19,7 +19,7 @@ export default function Chat() {
   const Navigate = useNavigate();
   const inputReference = useRef();
   const { roomNumber } = useParams();
-
+  const [loading, setLoading] = useState(true);
   const [isMenuOpen, setMenuOpen] = useState(false);
   const [selectedStatus, setSelectedStatus] = useState('거래중');
   const menuItems = ['예약중', '거래 완료', '거래중'];
@@ -37,6 +37,23 @@ export default function Chat() {
     }
   }, []);
   // Data loading
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Data loading and socket connection
+        const response = await axios({
+          url: `${apiUrl}/proxy/chat/${roomNumber}`,
+          method: 'GET',
+        });
+        setMessages(response.data.list);
+        console.log(response.data.list);
+        socket.emit('getRoomInfo', roomNumber);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchData();
+  }, [roomNumber, id]);
 
   // id 값이 업데이트될 때 소켓 이벤트 처리
   useEffect(() => {
@@ -61,7 +78,7 @@ export default function Chat() {
               setReceiver(data.receiver);
               setProxy(data.proxyData.photo);
               setProxyPayId(data.proxyData.proxyId);
-
+              setLoading(false);
               setWm(data.wmData);
               console.log('wm', data.wmData);
               console.log('sender안녕' + data.proxyData.photo);
@@ -70,7 +87,7 @@ export default function Chat() {
               setProxy(data.proxyData);
               setWm(data.wmData);
               setReceiver(data.sender);
-
+              setLoading(false);
               console.log('wm', data.wmData);
               console.log('receiver안녕' + data.proxyData.photo);
             }
@@ -115,7 +132,6 @@ export default function Chat() {
       // 입력값 초기화
       setInputValue('');
       inputReference.current.value = '';
-    } else {
     }
   };
   useEffect(() => {
@@ -152,15 +168,6 @@ export default function Chat() {
   const parseDate = (dateString) => {
     return dateString.slice(11, 16);
   };
-  useEffect(() => {
-    if (selectedStatus === '예약중') {
-      socket.emit('reserve', { wmId: wm.wmId, proxyId: proxyPayId });
-    } else if (selectedStatus === '거래중') {
-      socket.emit('deleteReservation', { wmId: wm.wmId });
-    } else if (selectedStatus === '거래 완료') {
-      socket.emit('completed', { wmId: wm.wmId, proxyId: proxyPayId });
-    }
-  }, [selectedStatus]);
 
   // const parseImgData = (dataStringImg) => {
 
@@ -189,94 +196,102 @@ export default function Chat() {
       console.error('wm나 proxy가 없습니다.');
     }
   };
-
+ 
   return (
     <div className="container">
-      <div>
-        <div className="initial-chat-container">
-          <p class="chat_header">
-            <button
-              onClick={toggleMenu}
-              className="font-extrabold text-primary py-2 px-1 sm:py-2 sm:px-1 md:py-2 md:px-2 text-xs sm:text-sm md:text-baserounded-full relative"
-            >
-              <div className="chat_header_status"> {selectedStatus} ▽</div>
-              {isMenuOpen && (
-                <div className="menu bg-gray-100 absolute right-0 top-full p-2 rounded-md shadow-md">
-                  {menuItems.map((item, index) => (
-                    <p
-                      className="p-2"
+      {!id ? (
+        <div>로그인을 먼저 해주시기 바랍니다...</div>
+      ) : (
+        <div>
+          {loading ? (
+            <div>로딩 중...</div>
+          ) : (
+            <div className="initial-chat-container">
+              <p class="chat_header">
+                <button
+                  onClick={toggleMenu}
+                  className="font-extrabold text-primary py-2 px-1 sm:py-2 sm:px-1 md:py-2 md:px-2 text-xs sm:text-sm md:text-baserounded-full relative"
+                >
+                  <div className="chat_header_status"> {selectedStatus} ▽</div>
+                  {isMenuOpen && (
+                    <div className="menu bg-gray-100 absolute right-0 top-full p-2 rounded-md shadow-md">
+                      {menuItems.map((item, index) => (
+                        <p
+                          className="p-2"
+                          key={index}
+                          onClick={() => {
+                            setSelectedStatus(item);
+                            setMenuOpen(false);
+                          }}
+                        >
+                          {item}
+                        </p>
+                      ))}
+                    </div>
+                  )}
+                </button>
+                현재 회원님의 아이디는 {sender.userId}입니다
+              </p>
+              <div className="message_container" ref={chatContainerRef}>
+                {messages.map((msg, index) => {
+                  console.log('msg', msg);
+                  console.log('msg.receiver:', msg.receiver);
+                  console.log('receiver.userId:', receiver.userId);
+                  console.log('proxy:', msg.photoData);
+                  console.log(
+                    'photo:',
+                    msg.receiver !== receiver.userId ? proxy.photo : null
+                  );
+                  return (
+                    // <div key={index} className={msg.sender === sender.userId ? 'me' : 'other'}>{`${msg.sender} ${msg.createdAt}`} === {msg.messageContent}</div>
+                    <MessageBox
                       key={index}
-                      onClick={() => {
-                        setSelectedStatus(item);
-                        setMenuOpen(false);
-                      }}
-                    >
-                      {item}
-                    </p>
-                  ))}
-                </div>
-              )}
-            </button>
-            현재 회원님의 아이디는 {sender.userId}입니다
-          </p>
-          <div className="message_container" ref={chatContainerRef}>
-            {messages.map((msg, index) => {
-              console.log('msg', msg);
-              console.log('msg.receiver:', msg.receiver);
-              console.log('receiver.userId:', receiver.userId);
-              console.log('proxy:', msg.photoData);
-              console.log(
-                'photo:',
-                msg.receiver !== receiver.userId ? proxy.photo : null
-              );
-              return (
-                // <div key={index} className={msg.sender === sender.userId ? 'me' : 'other'}>{`${msg.sender} ${msg.createdAt}`} === {msg.messageContent}</div>
-                <MessageBox
-                  key={index}
-                  className={msg.sender === sender.userId ? 'me' : 'other'}
-                  avatar={msg.receiver !== receiver.userId ? proxy.photo : null}
-                  type={msg.messageType}
-                  text={msg.messageContent}
-                  title={`${msg.sender} ${parseDate(msg.createdAt)}`}
-                  notch={false}
-                ></MessageBox>
-              );
-            })}
-          </div>
-          <div className="input_container">
-            <Input
-              className="input_item"
-              referance={inputReference}
-              multiline={true}
-              value={inputValue}
-              onChange={(e) => setInputValue(e.target.value)}
-              onKeyPress={(e) => {
-                if (e.key === 'Enter') {
-                  sendMessage();
-                }
-              }}
-              leftButtons={
-                id === userPayId && (
-                  <Button
-                    key="paymentButton"
-                    color="#4CAF50"
-                    backgroundColor="transparent"
-                    text="결제하기"
-                    onClick={PaymentsList}
-                  />
-                )
-              }
-              rightButtons={
-                <Button
-                  className="input_send_btn"
-                  backgroundColor="transparent"
-                  onClick={sendMessage}
+                      className={msg.sender === sender.userId ? 'me' : 'other'}
+                      avatar={msg.receiver !== receiver.userId ? proxy.photo : null}
+                      type={msg.messageType}
+                      text={msg.messageContent}
+                      title={`${msg.sender} ${parseDate(msg.createdAt)}`}
+                      notch={false}
+                    ></MessageBox>
+                  );
+                })}
+              </div>
+              <div className="input_container">
+                <Input
+                  className="input_item"
+                  referance={inputReference}
+                  multiline={true}
+                  value={inputValue}
+                  onChange={(e) => setInputValue(e.target.value)}
+                  onKeyPress={(e) => {
+                    if (e.key === 'Enter') {
+                      sendMessage();
+                    }
+                  }}
+                  leftButtons={
+                      id === userPayId && (
+                        <Button
+                          key="paymentButton"
+                          color="#4CAF50"
+                          backgroundColor="transparent"
+                          text="결제하기"
+                          onClick={PaymentsList}
+                        />
+                      )
+                  }
+                  rightButtons={
+                    <Button
+                      className="input_send_btn"
+                      backgroundColor="transparent"
+                      onClick={sendMessage}
+                    />
+                  }
                 />
-              }
-            />
-          </div>
+              </div>
+            </div>
+          )}
         </div>
-      </div>
+      )}
     </div>
   );
 }
