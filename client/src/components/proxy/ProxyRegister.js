37c638575e251ac911678a41ba69c6fd39099558
kim_jihyung <sy33002@naver.com>
2023-11-10 10:56:25 +0900

    feat: 수정중

--- client/src/components/proxy/ProxyRegister.js
+++ client/src/components/proxy/ProxyRegister.js
@@ -46,7 +46,7 @@ export default function ProxyRegister() {
     const formData = new FormData();
     formData.append('proxyAddress', combinedAddress);
     formData.append('title', data.title);
-    formData.append('id', 2);
+    formData.append('id', id);
     formData.append('gender', data.gender);
     formData.append('age', data.age);
     formData.append('proxyMsg', data.proxyMsg);
