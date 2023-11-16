1348249fe328c2206041d27d5dab339480ff5295
LOBSTER10000 <ghh112034@gmail.com>
2023-11-16 23:19:25 +0900

    fix : 채팅창 밸리데이션 수정

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,7 +3,10 @@ import { io } from 'socket.io-client';
 let socketInstance = null;
 export const getSocket = () => {
   if (!socketInstance) {
-    socketInstance = io(`http://localhost:8080`);
+    socketInstance = io(`https://sesac-projects.site/`, {
+      path: '/wapi/socket.io',
+      withCredentials : true,
+    });
   }
   return socketInstance;
 };
